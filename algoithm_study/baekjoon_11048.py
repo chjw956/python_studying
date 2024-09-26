141febf88bdef7624d9e725d17f4cc8f767ae8bd
최지원 <chjw956@naver.com>
2024-09-26 16:20:17 +0900

    [Solve] Baekjoon 11048.이동하기

--- algoithm_study/baekjoon_11048.py
+++ algoithm_study/baekjoon_11048.py
@@ -3,9 +3,29 @@
 # 준규가 가져올 수 있는 사탕의 최대 개수를 구하라.
 # (단, 이동 방향은 오른쪽, 아래쪽, 대각선 오른쪽 아래 방향으로만 가능하다.)
 
+import sys
+sys.stdin = open('sample_input\sample_input(21).txt', 'r')
+
+directions = [[1, 0], [0, 1], [1, 1]]
+
 T = int(input())
 
 for tc in range(1, T + 1):
     N, M = map(int, input().split())
-    miro = [list(map(int, input().split())) for _ in range(N)]
+    miro = [[0] * (M + 1)]
 
+    for _ in range(N):
+        miro.append([0] + list(map(int, input().split())))
+
+    # 이전 값을 저장할 dp 테이블 생성
+    d = [[0] * (M + 1) for _ in range(N + 1)]
+
+    # 출발점
+    si = sj = 1
+    d[si][sj] = miro[si][sj]
+
+    for i in range(1, N + 1):
+        for j in range(1, M + 1):
+            d[i][j] = max(d[i-1][j], d[i][j-1], d[i-1][j-1]) + miro[i][j]
+
+    print(d[N][M])
--- algoithm_study/nadongbin_dp_5.py
+++ algoithm_study/nadongbin_dp_5.py
@@ -2,5 +2,24 @@
 # N종류의 화폐가 있을 때, 화폐의 개수를 최소로 하여 그 가치의 합이 M원이 되도록 하려고 한다.
 
 N, M = map(int, input().split())
-money = list(map(int, input() for _ in range(N)))
+money = []
+for _ in range(N):
+    money += [int(input())]
+money.sort(reverse=True)
 
+d = [float('inf')] * (M + 1)
+
+for m in money:
+    d[m] = 1
+
+for n in range(money[-1], M + 1):
+    if d[n] == float('inf'):
+        for m in money:
+            if n - m >= money[-1] and d[n - m] != float('inf'):
+                d[n] = min(d[n], d[n - m] + 1)
+                break
+
+if d[M] == float('inf'):
+    print(-1)
+else:
+    print(d[M])
